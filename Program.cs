719736c29dfca82ed5522d2ea2d6863f97bf631e
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-22 22:48:35 +0200

    lesson 3

--- Program.cs
+++ Program.cs
@@ -10,21 +10,22 @@ namespace Zmeik4
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym = '#';
+            Point p1=new Point();
+            p1.x = 1; 
+            p1.y = 1;
+            p1.sym = '#';
+            p1.Draw();
 
-            Draw(x1, y1, sym);
+            Point p2=new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '¤';
+            p2.Draw();
+
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-			Console.Write(sym);
 
-
-        }
     }
 
 }
