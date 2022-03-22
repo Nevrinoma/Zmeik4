a2f13ff4dbcaadd46b026964aad69e4281610573
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-22 23:15:35 +0200

    lesson 6

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Zmeik4
 {
-    internal class Program
+	internal class Program
     {
         static void Main(string[] args)
         {
@@ -16,6 +13,25 @@ namespace Zmeik4
             Point p2=new Point(4,5,'¤');
             p2.Draw();
 
+            List<int> numList=new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+			foreach (int i in numlist)
+			{
+				Console.WriteLine(i);
+			}
+
+            numList.Remove(0);
+
+            List<int> pList = new List<int>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
