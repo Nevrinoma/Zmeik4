6663b5d99d92938b26b0d1b6c1817e21bbe483c3
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-23 02:33:38 +0200

    lesson 10

--- Point.cs
+++ Point.cs
@@ -52,6 +52,12 @@ namespace Zmeik4
 			Console.Write(sym);
 		}
 
+		public void Clear()
+		{
+			sym = ' ';
+			Draw();
+		}
+
 		public override string ToString()
 		{
 			return x + ", " + y + ", " + sym;
