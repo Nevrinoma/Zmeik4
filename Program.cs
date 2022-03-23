fae83aabfdd93dc7f2239ab0633b80a475b12820
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-23 04:16:10 +0200

    game over

--- Program.cs
+++ Program.cs
@@ -43,8 +43,33 @@ namespace Zmeik4
 					ConsoleKeyInfo key = Console.ReadKey();
 					snake.HandleKey(key.Key);
 				}
+
+
+
+
 			}
+			WriteGameOver();
+			Console.ReadLine();
 		}
-    }
+		static void WriteGameOver()// функция которая выводит информацию после поражения в игре
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("G A M E     O V E R", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("                     ", xOffset + 2, yOffset++);
+			WriteText("Made by Nevrinoma", xOffset + 1, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+	}
 
 }
