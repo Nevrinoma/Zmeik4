cb0222c1dcc78709a3673c1e98b8f1ce1d58c2b1
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-23 03:52:33 +0200

    lesson 14

--- Program.cs
+++ Program.cs
@@ -6,71 +6,44 @@ namespace Zmeik4
 {
 	internal class Program
     {
-        static void Main(string[] args)
-        {
-            //Console.SetWindowSize(81, 41);
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(81, 41);
 
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
+			Point p = new Point(4, 5, '#');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-
-
-
-
-
-
-
-            Point p=new Point(4,5,'¤');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
-
-            FoodCreator foodCreator = new FoodCreator(82, 42, '&');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+			FoodCreator foodCreator = new FoodCreator(82, 42, '&');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
 			while (true)
 			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
 				if (snake.Eat(food))
 				{
-                    food = foodCreator.CreateFood();
-                    food.Draw();
+					food = foodCreator.CreateFood();
+					food.Draw();
 				}
 				else
 				{
-                    snake.Move();
+					snake.Move();
 				}
-                Thread.Sleep(100);
-			}
 
-			while (true)
-			{
+				Thread.Sleep(100);
 				if (Console.KeyAvailable)
 				{
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-
-                }
-                Thread.Sleep(100);
-                snake.Move();
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
 			}
-            /*
-            HorizontalLine upLine=new HorizontalLine(0,80,0,'#');
-            HorizontalLine downLine = new HorizontalLine(0, 80, 40, '#');
-            VerticalLine leftLine = new VerticalLine(0, 40, 0, '#');
-            VerticalLine rightLine = new VerticalLine(0, 40, 80, '#');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-            */  
-
-
-            Console.ReadLine();
-        }
-        static void Draw(Figure figure)
-		{
-            figure.Draw();
 		}
     }
 
