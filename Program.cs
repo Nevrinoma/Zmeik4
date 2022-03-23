9559c615618983e1ec2af45ecbd6412000aac25b
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-23 02:51:37 +0200

    lesson 11

--- Program.cs
+++ Program.cs
@@ -13,17 +13,18 @@ namespace Zmeik4
             Point p=new Point(4,5,'¤');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+
+                }
+                Thread.Sleep(100);
+                snake.Move();
+			}
 
             HorizontalLine upLine=new HorizontalLine(0,80,0,'#');
             HorizontalLine downLine = new HorizontalLine(0, 80, 40, '#');
